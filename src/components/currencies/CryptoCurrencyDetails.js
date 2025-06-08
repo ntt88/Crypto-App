cb2cf6e2bd7f8c4f74baee887fe2d4823887a998
ntt88 <hmt88@gmx.com>
2025-06-08 12:14:19 -0600

    adding another api call

--- src/components/currencies/CryptoCurrencyDetails.js
+++ src/components/currencies/CryptoCurrencyDetails.js
@@ -1,7 +1,17 @@
 import React from 'react';
 // import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+async function myFunction() {
+  // const apiUrl = 'https://api.example.com/data';
+  axios.get('https://api.coinlore.net/api/tickers/').then((response) => {
+    console.log(response);
+  }).catch((err) => console.log(err));
+}
 
 const CryptoCurrencyDetails = ({ currency }) => {
+  const data = myFunction();
+  console.log(data);
   // const navigate = useNavigate();
   console.log(currency);
   return (
