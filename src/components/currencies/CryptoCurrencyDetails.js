3eb8ad44b624231fdcfedf6f8aa085271570cb3a
sh <Hmt88@gmx.com>
2025-08-23 15:30:08 -0600

    working on a new page

--- src/components/currencies/CryptoCurrencyDetails.js
+++ src/components/currencies/CryptoCurrencyDetails.js
@@ -1,29 +1,40 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-// import { useNavigate } from 'react-router-dom';
-// import axios from 'axios';
-/*
-async function myFunction() {
-  let data;
-  // const apiUrl = 'https://api.example.com/data';
-  axios.get('https://api.coinlore.net/api/tickers/').then((response) => {
-    console.log(response);
-    data = response;
-  }).catch((err) => console.log(err));
-  return data;
-}
-*/
+
+const findCurrencey = (symbol, currencies) => {
+  console.log(symbol);
+  console.log(currencies);
+  const keys = Object.keys(currencies);
+  console.log(keys);
+  for (let i = 0; i < keys.length; i += 1) {
+    console.log(currencies[i].symbol);
+    if (currencies[i].symbol === symbol) {
+      return currencies[i];
+    }
+  }
+  return null;
+};
 
 const CryptoCurrencyDetails = () => {
   const location = useLocation();
   const { currency, currencies } = location.state;
-  // const navigate = useNavigate();
   console.log('currency:', currency);
   console.log('currencies:', currencies);
+  const curr = findCurrencey('BTC', currencies);
+  console.log(curr);
   return (
     <div>
-      <h2 className="symbol">TESTING</h2>
-      <p className="change">TEST</p>
+      <h2 className="symbol">
+        {currency.name}
+      </h2>
+      <p className="change">
+        Market Cap: $
+        {currency.market_cap_usd}
+      </p>
+      <h2 className="symbol">
+        { parseFloat(((currency.market_cap_usd / curr.market_cap_usd) * 100).toFixed(3)) }
+        % of Bitcoins Market Cap in USD
+      </h2>
     </div>
   );
 };
