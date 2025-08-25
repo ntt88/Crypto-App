70567016f00b00c45defe1e3c877ae9245573e21
sh <Hmt88@gmx.com>
2025-08-25 11:00:26 -0600

    the bar chart is now working

--- src/components/currencies/CryptoCurrencyDetails.js
+++ src/components/currencies/CryptoCurrencyDetails.js
@@ -1,5 +1,53 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import faker from 'faker';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
 
 const findCurrencey = (symbol, currencies) => {
   console.log(symbol);
@@ -20,22 +68,10 @@ const CryptoCurrencyDetails = () => {
   const { currency, currencies } = location.state;
   console.log('currency:', currency);
   console.log('currencies:', currencies);
-  const curr = findCurrencey('BTC', currencies);
-  console.log(curr);
+  const BTC = findCurrencey('BTC', currencies);
+  console.log(BTC);
   return (
-    <div>
-      <h2 className="symbol">
-        {currency.name}
-      </h2>
-      <p className="change">
-        Market Cap: $
-        {currency.market_cap_usd}
-      </p>
-      <h2 className="symbol">
-        { parseFloat(((currency.market_cap_usd / curr.market_cap_usd) * 100).toFixed(3)) }
-        % of Bitcoins Market Cap in USD
-      </h2>
-    </div>
+    <Bar options={options} data={data} />
   );
 };
 
