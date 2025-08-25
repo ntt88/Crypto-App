ec1defdf1ffd54b96caacba12e865818ef454e9a
sh <Hmt88@gmx.com>
2025-08-25 11:47:50 -0600

    comparing crypto market caps

--- src/components/currencies/CryptoCurrencyDetails.js
+++ src/components/currencies/CryptoCurrencyDetails.js
@@ -12,6 +12,20 @@ import {
 import { Bar } from 'react-chartjs-2';
 // import faker from 'faker';
 
+const findCurrencey = (symbol, currencies) => {
+  console.log(symbol);
+  console.log(currencies);
+  const keys = Object.keys(currencies);
+  console.log(keys);
+  for (let i = 0; i < keys.length; i += 1) {
+    console.log(currencies[i].symbol);
+    if (currencies[i].symbol === symbol) {
+      return currencies[i];
+    }
+  }
+  return null;
+};
+
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -31,50 +45,35 @@ export const options = {
   },
 };
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: {
-        January: 614, February: 704, March: 544, April: 243, May: 790, June: 235, July: 969,
-      },
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-    {
-      label: 'Dataset 2',
-      data: {
-        January: 614, February: 704, March: 544, April: 243, May: 790, June: 235, July: 969,
-      },
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    },
-  ],
-};
-
-const findCurrencey = (symbol, currencies) => {
-  console.log(symbol);
-  console.log(currencies);
-  const keys = Object.keys(currencies);
-  console.log(keys);
-  for (let i = 0; i < keys.length; i += 1) {
-    console.log(currencies[i].symbol);
-    if (currencies[i].symbol === symbol) {
-      return currencies[i];
-    }
-  }
-  return null;
-};
-
 const CryptoCurrencyDetails = () => {
   const location = useLocation();
   const { currency, currencies } = location.state;
   console.log('currency:', currency);
   console.log('currencies:', currencies);
-  console.log('data:', data);
+  const labels = ['BTC', 'ETH', 'CRO', 'ADA', 'ATOM', 'SOL', 'LTC'];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: {
+          BTC: findCurrencey('BTC', currencies).market_cap_usd, ETH: findCurrencey('ETH', currencies).market_cap_usd, CRO: findCurrencey('CRO', currencies).market_cap_usd, ADA: findCurrencey('ADA', currencies).market_cap_usd, ATOM: findCurrencey('ATOM', currencies).market_cap_usd, SOL: findCurrencey('SOL', currencies).market_cap_usd, LTC: findCurrencey('LTC', currencies).market_cap_usd,
+        },
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: 'Dataset 2',
+        data: {
+          BTC: findCurrencey('BTC', currencies).market_cap_usd, ETH: findCurrencey('ETH', currencies).market_cap_usd, CRO: findCurrencey('CRO', currencies).market_cap_usd, ADA: findCurrencey('ADA', currencies).market_cap_usd, ATOM: findCurrencey('ATOM', currencies).market_cap_usd, SOL: findCurrencey('SOL', currencies).market_cap_usd, LTC: findCurrencey('LTC', currencies).market_cap_usd,
+        },
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
   const BTC = findCurrencey('BTC', currencies);
   console.log(BTC);
+  console.log('data:', data);
   return (
     <Bar options={options} data={data} />
   );
